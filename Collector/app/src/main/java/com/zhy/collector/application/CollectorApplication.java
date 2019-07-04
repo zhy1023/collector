8436b78f2fed41834d35556c37368db7317e5757
zhangyuan <zhangyuan@oray.com>
2019-07-04 15:44:26 +0800

    new project

---
+++ Collector/app/src/main/java/com/zhy/collector/application/CollectorApplication.java
@@ -0,0 +1,12 @@
+package com.zhy.collector.application;
+
+import android.app.Application;
+
+/**
+ * Created by ZY on 2019/7/4
+ * DESC: class CollectorApplication
+ */
+public class CollectorApplication extends Application {
+
+
+}
---
+++ Collector/app/src/main/java/com/zhy/collector/constants/AppConstant.java
@@ -0,0 +1,8 @@
+package com.zhy.collector.constants;
+
+/**
+ * Created by ZY on 2019/7/4
+ * DESC: class AppConstant
+ */
+public interface AppConstant {
+}
