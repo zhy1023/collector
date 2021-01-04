f53d7bed4560cdbdda763255c52c7122ba447627
zhy <13584851153@163.com>
2021-01-04 18:46:03 +0800

    算法测试

---
+++ Collector/lib_library/src/main/java/com/zhy/lib_library/arithmetic/LinkedList.java
@@ -0,0 +1,11 @@
+package com.zhy.lib_library.arithmetic;
+
+/**
+ * @Author ；zhy
+ * @ClassName: LinkedList
+ * @Date : 2020/12/24 17:23
+ * @Describe :
+ */
+public class LinkedList {
+
+}
---
+++ Collector/lib_library/src/main/java/com/zhy/lib_library/arithmetic/MainClass.java
@@ -0,0 +1,17 @@
+package com.zhy.lib_library.arithmetic;
+
+/**
+ * @Author ；zhy
+ * @ClassName: MainClass
+ * @Date : 2020/12/29 15:21
+ * @Describe : MainClass
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+//        System.out.println("isUniqueStr :" + Utils.isUniqueStr("asfcdea"));
+//        System.out.println("compressStr :" + Utils.compressStr("aaaafcccccdeeeeh"));
+//        System.out.println("oneEditAway :" + Utils.oneEditAway("pales","pal"));
+        System.out.println("canPermutePalindrome :" + Utils.canPermutePalindrome("aab"));
+    }
+}
